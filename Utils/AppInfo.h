50be169516ae16288b5894892937385f194fb8ad
qufei <qufei@mapbar.com>
2016-01-10 08:49:26 +0800

    Add
    1.add third part : effect image
    2.add utils:App info

---
+++ Utils/AppInfo.h
@@ -0,0 +1,13 @@
+//
+//  AppInfo.h
+//  SuperPaper
+//
+//  Created by 瞿飞 on 16/1/10.
+//  Copyright © 2016年 Share technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppInfo : NSObject
+
+@end
---
+++ Utils/AppInfo.m
@@ -0,0 +1,13 @@
+//
+//  AppInfo.m
+//  SuperPaper
+//
+//  Created by 瞿飞 on 16/1/10.
+//  Copyright © 2016年 Share technology. All rights reserved.
+//
+
+#import "AppInfo.h"
+
+@implementation AppInfo
+
+@end
