c87bd4b84e86b5294f3b53ca46dfb7b3da7e9574
lihaoEmily <1611045443@qq.com>
2016-01-13 14:53:32 +0800

    临时提交

---
+++ SuperPaper/Study/PublicationSearchTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  PublicationSearchTableViewCell.h
+//  SuperPaper
+//
+//  Created by lihao on 16/1/13.
+//  Copyright © 2016年 Share technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PublicationSearchTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) UIImageView *iconImg;
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@end
