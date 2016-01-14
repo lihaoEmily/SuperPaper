aefede85ad96d4a480ca041b383d4721fbb4553e
lihaoEmily <1611045443@qq.com>
2016-01-14 10:43:09 +0800

    完善刊物搜索页面

--- SuperPaper/Study/PublicationSearchViewController.m
+++ SuperPaper/Study/PublicationSearchViewController.m
@@ -8,6 +8,7 @@
 
 #import "PublicationSearchViewController.h"
 #import "PublicationSearchTableViewCell.h"
+#import "AFNetworking.h"
 
 @interface PublicationSearchViewController ()<UITableViewDataSource, UITableViewDelegate>
 
@@ -17,25 +18,29 @@
 {
     NSArray *_iconArr;
     NSArray *_titleArr;
+    NSString *_bundleStr;
+    UITextField *_searchBar;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    _bundleStr = [[NSBundle mainBundle] pathForResource:@"Resources" ofType:@"bundle"];
     [self setupUI];
     
     _iconArr = @[@"searchList1", @"searchList2", @"searchList3", @"searchList1", @"searchList2", @"searchList3"];
     _titleArr = @[@"马克思主义与现实 Marxism & Reality", @"外国文学评论 Foreign Literature Review", @"外国文学评论 Foreign Literature Review", @"马克思主义与现实 Marxism & Reality", @"外国文学评论 Foreign Literature Review", @"外国文学评论 Foreign Literature Review"];
 }
 
+- (void)getData
+{
+    
+}
+
 // UI搭建
 - (void)setupUI
 {
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[[UIImage imageNamed:@"backnew"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate] style:UIBarButtonItemStylePlain target:self action:@selector(back)];
-    
     self.title = @"刊物搜索";
-    self.view.backgroundColor = [UIColor colorWithRed:180.0 /255.0 green:180.0 /255.0 blue:180.0 /255.0 alpha:1.0];
-    
     [self setupSearchBar];
     [self setupTableView];
 }
@@ -43,27 +48,48 @@
 // 配置搜索框
 - (void)setupSearchBar
 {
-    UITextField *searchBar = [[UITextField alloc] initWithFrame:CGRectMake(15, 5, self.view.frame.size.width - 30, 40)];
-    searchBar.backgroundColor = [UIColor whiteColor];
-    searchBar.layer.cornerRadius = 5;
-    searchBar.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    // searchBar的容器View
+    UIView *searchBgView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+    [self.view addSubview:searchBgView];
     
-    UIImageView *leftView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
-    leftView.image = [UIImage imageNamed:@"search"];
-    leftView.contentMode = UIViewContentModeCenter;
-    searchBar.leftView = leftView;
-    searchBar.leftViewMode = UITextFieldViewModeAlways;
+    // 灰色背景图片
+    UIImageView *searchBgImg = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+    UIImage *image = [UIImage imageNamed:[[NSBundle bundleWithPath:_bundleStr] pathForResource:@"searchBg" ofType:@"png" inDirectory:@"temp"]];
+    searchBgImg.image = image;
+    [searchBgView addSubview:searchBgImg];
+    searchBgImg.userInteractionEnabled = YES;
     
-    UIButton *rightView = [UIButton buttonWithType:UIButtonTypeCustom];
-    rightView.frame = CGRectMake(searchBar.frame.size.width - 80, 0, 80, searchBar.frame.size.height);
-    [rightView setTitle:@"搜一下" forState:UIControlStateNormal];
-    [rightView setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    rightView.backgroundColor = [UIColor colorWithRed:232/255.0 green:79/255.0 blue:135./255.0 alpha:1.0f];
-    searchBar.rightView = rightView;
-    searchBar.rightViewMode = UITextFieldViewModeAlways;
+    // 白色背景图片
+    UIImageView *searchBarImg = [[UIImageView alloc] initWithFrame:CGRectMake(15, 5, self.view.frame.size.width - 30, 40)];
+    UIImage *searchBarImage = [UIImage imageNamed:[[NSBundle bundleWithPath:_bundleStr] pathForResource:@"searchBar" ofType:@"png" inDirectory:@"temp"]];
+    searchBarImg.image = searchBarImage;
+    [searchBgImg addSubview:searchBarImg];
+    searchBarImg.userInteractionEnabled = YES;
     
-    searchBar.clearButtonMode = UITextFieldViewModeAlways;
-    [self.view addSubview:searchBar];
+    // 左侧图片搜索button
+    UIButton *searchIconBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    searchIconBtn.frame = CGRectMake(10, 5, 30, 30);
+    UIImage *searchIconBtnImage = [UIImage imageNamed:[[NSBundle bundleWithPath:_bundleStr] pathForResource:@"searchIcon" ofType:@"png" inDirectory:@"temp"]];
+    [searchIconBtn setBackgroundImage:searchIconBtnImage forState:UIControlStateNormal];
+    [searchIconBtn addTarget:self action:@selector(clickToShowKeyboard) forControlEvents:UIControlEventTouchUpInside];
+    [searchBarImg addSubview:searchIconBtn];
+    
+    // 搜索textfield
+    _searchBar = [[UITextField alloc] initWithFrame:CGRectMake(50, 0, searchBarImg.frame.size.width - 50, 40)];
+    _searchBar.layer.cornerRadius = 5;
+    _searchBar.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    
+    // 右侧文字搜索button
+    UIButton *searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    searchBtn.frame = CGRectMake(_searchBar.frame.size.width - 80, 0, 80, _searchBar.frame.size.height);
+    _searchBar.placeholder = @"输入您要搜索的关键词";
+    [searchBtn setTitle:@"搜一下" forState:UIControlStateNormal];
+    [searchBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [searchBtn addTarget:self action:@selector(clickToSearch) forControlEvents:UIControlEventTouchUpInside];
+    searchBtn.backgroundColor = [UIColor colorWithRed:232/255.0 green:79/255.0 blue:135./255.0 alpha:1.0f];
+    _searchBar.rightView = searchBtn;
+    _searchBar.rightViewMode = UITextFieldViewModeAlways;
+    [searchBarImg addSubview:_searchBar];
 }
 
 // 配置tableView
@@ -73,15 +99,29 @@
     tableView.showsVerticalScrollIndicator = NO;
     tableView.delegate = self;
     tableView.dataSource = self;
+    tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
     [self.view addSubview:tableView];
 }
 
+#pragma mark - Actions
 - (void)back
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-#pragma mark - UITableView DataSource
+- (void)clickToShowKeyboard
+{
+    [_searchBar resignFirstResponder];
+    NSLog(@"clickToShowKeyboard");
+}
+
+- (void)clickToSearch
+{
+    [_searchBar resignFirstResponder];
+    NSLog(@"clickToSearch");
+}
+
+#pragma mark - UITableView DataSource and Delegate
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return _iconArr.count;
@@ -94,7 +134,9 @@
     if (!cell) {
         cell = [[PublicationSearchTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
     }
-    [cell.iconImg setImage:[UIImage imageNamed:[_iconArr objectAtIndex:indexPath.row]]];
+    
+    UIImage *image = [UIImage imageNamed:[[NSBundle bundleWithPath:_bundleStr] pathForResource:[_iconArr objectAtIndex:indexPath.row] ofType:@"png" inDirectory:@"temp"]];
+    [cell.iconImg setImage:image];
     cell.titleLabel.text = [_titleArr objectAtIndex:indexPath.row];
     return cell;
 }
@@ -102,6 +144,12 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     return 90;
+}
+
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [_searchBar resignFirstResponder];
 }
 
 @end
