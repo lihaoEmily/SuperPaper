b72a74dbbf8dc4960bc2e8946f95d7bcf2ff3c13
qudd <wendysgirls@163.com>
2016-01-19 16:48:01 +0800

    修改字体大小，添加默认图片

--- SuperPaper/Main/MainViewController.m
+++ SuperPaper/Main/MainViewController.m
@@ -95,6 +95,8 @@
     self.currentController = self.homeController;
     
     self.navigationItem.rightBarButtonItem = self.normalBarButtonItem;
+    self.tabbar.tabBarDisplayType = TabBarDisplayTypeStudent;
+
 }
 
 - (void)userAction:(UIButton *)button
