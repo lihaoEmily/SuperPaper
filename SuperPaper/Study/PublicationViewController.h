559309c45eabe0e4129c0a4836f9b2119df70c14
liuchang <liuchang@mapbar.com>
2016-01-19 13:06:54 +0800

    重新下载新的分支，新建刊物VC类文件

---
+++ SuperPaper/Study/PublicationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PublicationViewController.h
+//  SuperPaper
+//
+//  Created by admin on 16/1/19.
+//  Copyright © 2016年 Share technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PublicationViewController : UIViewController
+
+@end
