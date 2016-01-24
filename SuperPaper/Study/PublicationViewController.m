b4b801e620ef342923d11c1f646670ea4eddd406
liuchang <liuyueliuchang@163.com>
2016-01-24 17:03:43 +0800

    刊物界面

--- SuperPaper/Study/PublicationViewController.m
+++ SuperPaper/Study/PublicationViewController.m
@@ -8,14 +8,25 @@
 
 #import "PublicationViewController.h"
 
-@interface PublicationViewController ()<UITableViewDataSource,UITableViewDelegate>{
-    UITableView *_leftTable;
-    UITableView *_rightTable;
-}
+@interface PublicationViewController ()<UITableViewDataSource,UITableViewDelegate>
 
 @end
 
 @implementation PublicationViewController
+{
+    UITableView* _leftTable;
+    UITableView* _rightTable;
+    NSIndexPath* _selectedCellIndex;
+    NSString* _bundleStr;
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _selectedCellIndex = nil;
+        _bundleStr = [[NSBundle mainBundle] pathForResource:@"Resources" ofType:@"bundle"];
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,31 +35,64 @@
     _leftTable.frame = CGRectMake(0, 0, self.view.bounds.size.width/4, self.view.bounds.size.height);
     _leftTable.dataSource = self;
     _leftTable.delegate = self;
-//    _leftTable.backgroundColor = [UIColor grayColor];
 //    _leftTable.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     _leftTable.showsVerticalScrollIndicator = NO;
 //    _leftTable.tableFooterView = [[UIView alloc] init];
     _leftTable.tag = 1000;
     _leftTable.separatorColor = [UIColor whiteColor];
-    _leftTable.separatorInset = UIEdgeInsetsMake(0,0, 0, 0);
-    _leftTable.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
-    if ([_tableView respondsToSelector:@selector(setSeparatorInset:)]) {
-        [_leftTable setSeparatorInset:UIEdgeInsetsZero];
-    }
-    if ([_tableView respondsToSelector:@selector(setLayoutMargins:)]) {
-        [_tableView setLayoutMargins:UIEdgeInsetsZero];
-    }
+    _leftTable.allowsSelection = YES;
+    _leftTable.allowsMultipleSelection = NO;
     [self.view addSubview:_leftTable];
     
     _rightTable = [[UITableView alloc] init];
     _rightTable.frame = CGRectMake(self.view.bounds.size.width/4, 0, self.view.bounds.size.width*3/4, self.view.bounds.size.height);
     _rightTable.dataSource = self;
     _rightTable.delegate = self;
-//    _rightTable.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     _rightTable.showsVerticalScrollIndicator = YES;
-//    _rightTable.tableFooterView = [[UIView alloc] init];
     _rightTable.tag = 2000;
     [self.view addSubview:_rightTable];
+    
+    [self loadNavigationView];
+}
+
+-(void)viewDidLayoutSubviews
+{
+    if ([_leftTable respondsToSelector:@selector(setSeparatorInset:)]) {
+        [_leftTable setSeparatorInset:UIEdgeInsetsMake(0,0,0,0)];
+        [_rightTable setSeparatorInset:UIEdgeInsetsMake(0,0,0,0)];
+    }
+    
+    if ([_leftTable respondsToSelector:@selector(setLayoutMargins:)]) {
+        [_leftTable setLayoutMargins:UIEdgeInsetsMake(0,0,0,0)];
+        [_rightTable setLayoutMargins:UIEdgeInsetsMake(0,0,0,0)];
+    }
+}
+
+- (void)loadNavigationView
+{
+    UIImage *image = [UIImage imageNamed:[[NSBundle bundleWithPath:_bundleStr] pathForResource:@"searchBtn" ofType:@"png" inDirectory:@"Paper"]];
+    UIButton *searchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    searchBtn.frame = CGRectMake(10, 0, 25, 25);
+    [searchBtn setImage:image forState:UIControlStateNormal];
+    [searchBtn addTarget:self action:@selector(searchPublication:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *searchItem = [[UIBarButtonItem alloc] initWithCustomView:searchBtn];
+    
+    image = [UIImage imageNamed:[[NSBundle bundleWithPath:_bundleStr] pathForResource:@"sortBtn" ofType:@"png" inDirectory:@"Paper"]];
+    UIButton *sortBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    sortBtn.frame = CGRectMake(10, 0, 25, 25);
+    [sortBtn setImage:image forState:UIControlStateNormal];
+    [sortBtn addTarget:self action:@selector(sortPublication:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *sortItem = [[UIBarButtonItem alloc] initWithCustomView:sortBtn];
+    
+    self.navigationItem.rightBarButtonItems = @[searchItem, sortItem];
+}
+
+- (void) searchPublication :(id) sender{
+    
+}
+
+- (void) sortPublication:(id) sender{
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -61,30 +105,31 @@
 {
     if (tableView.tag == 1000) {
         return 5;
-    }else{
-        return 10;
+    }
+    else{
+        return 100;
     }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (tableView.tag == 1000) {
-        static NSString *CellIdentifier = @"Cell";
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        static NSString *CellIdentifierPublicationLeft = @"leftCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifierPublicationLeft];
         
         if (cell == nil) {
-            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifierPublicationLeft];
         }
-        
         cell.textLabel.text = @"left";
         cell.backgroundColor = [UIColor grayColor];
         return cell;
-    }else{
-        static NSString *CellIdentifier2 = @"Cell2";
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier2];
+    }
+    else{
+        static NSString *CellIdentifierPublicationRight = @"rightCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifierPublicationRight];
         
         if (cell == nil) {
-            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier2];
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifierPublicationRight];
         }
         
         cell.textLabel.text = @"right";
@@ -93,17 +138,28 @@
 }
 
 #pragma mark - tableView 代理方法
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (tableView.tag == 1000) {
-//        [_categoryView show];
-    }
-    else{
-        NSLog(@"tap right tableview index:%ld",(long)indexPath.row);
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsZero];
     }
     
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (tableView.tag == 1000) {
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+//        cell.backgroundColor = [UIColor whiteColor];
+//        _selectedCellTag = indexPath;
+    }
+    else{
+//        NSLog(@"tap right tableview index:%ld",(long)indexPath.row);
+    }
+}
 
 @end
