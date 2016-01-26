b4a1f0f383387b06d088823605e6559d5c3c3ef9
qufei <qufei@mapbar.com>
2016-01-26 21:17:41 +0800

    添加刊物View

--- SuperPaper/AssessmentTitle/NormalAssessmentViewController.h
+++ SuperPaper/AssessmentTitle/NormalAssessmentViewController.h
@@ -2,7 +2,7 @@
 //  NormalAssessmentViewController.h
 //  SuperPaper
 //
-//  Created by mapbarDaLian on 16/1/21.
+//  Created by AppStudio on 16/1/21.
 //  Copyright © 2016年 Share technology. All rights reserved.
 //
 
