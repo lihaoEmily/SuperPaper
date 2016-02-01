7e33a45b3faec96c9e6556b5955d84d651cab206
yudt <yudt@mapbar.com>
2016-02-01 09:59:09 +0800

    ...

---
+++ SuperPaper/User/PersonalInfoHasSelectionTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  PersonalInfoHasSelectionTableViewCell.h
+//  SuperPaper
+//
+//  Created by  mapbar_ios on 16/1/15.
+//  Copyright © 2016年 Share technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonalInfoHasSelectionTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *detailsLabel;
+
+
+@end
